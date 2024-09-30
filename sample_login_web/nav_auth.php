82e5c3e3d767b97c15e091cf233819badbe6e5e5
CodexkHaeL <michaeljustine1111@proton.me>
2024-09-30 16:05:19 +0800

    Create nav_auth.php

---
+++ sample_login_web/nav_auth.php
@@ -0,0 +1,7 @@
+<nav>
+        <div class="logo">CodeXkHaeL</div>
+        <ul>
+            <li><a href="logout.php">Logout</a></li>
+            <li><a href="index.php">home</a></li>
+        </ul>
+    </nav>
