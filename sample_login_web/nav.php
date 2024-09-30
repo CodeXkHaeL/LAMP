17c79088b94841e1976ce3cbd9cd140d52e9cbbc
CodexkHaeL <michaeljustine1111@proton.me>
2024-09-30 16:05:53 +0800

    Create nav.php

---
+++ sample_login_web/nav.php
@@ -0,0 +1,7 @@
+<nav>
+        <div class="logo">CodeXkHaeL</div>
+        <ul>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+        </ul>
+    </nav>
