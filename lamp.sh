3ab91b46bb92cdd6d99cc9d5114df1927408f56b
CodexkHaeL <michaeljustine1111@proton.me>
2024-10-02 12:30:13 +0800

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-# Display a welcome message and start the installation process
+words=("This is the summary of the script" 
+       "* Updating the package list" 
+       "* Installing Apache2" 
+       "* Installing MySQL database" 
+       "* Installing PHP with extensions" 
+       "* Installing phpMyAdmin and configuration" 
+       "* Enabling the mbstring extension for PHP" 
+       "* Creating the database and tables for testing and creating an account with admin access" 
+       "* Creating a database connection for web testing")
+
+for word in "${words[@]}"; do
+    echo "$word"    
+    sleep 1.5
+done
+sleep 2
+clear
+
+
 echo "Automating the LAMP stack installation with phpMyAdmin!"
 sleep 2
 clear
@@ -75,13 +92,40 @@ EOL
 sudo a2enconf phpmyadmin
 sudo systemctl restart apache2
 
+#Creating Databse and table for database testing and creating account with admin access 
 read -p "please enter you're username for mysql login:" username
-read -sp "please enter youre password:" pass
+read -sp "please enter youre password:" password
 sudo mysql <<EOF
 CREATE USER '$username'@'localhost' IDENTIFIED BY '$pass';
 GRANT ALL PRIVILEGES ON *.* TO '$username'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EOF
 clear
-echo "LAMP stack and phpMyAdmin installation complete! Enjoy your setup!"
+echo "Creating database connection for web test later.."
 sleep 2
+clear
+
+
+
+# creating db connection for web test
+directory="/var/www/html/"
+sudo cat <<EOF> "$directory/db.php"
+<?php
+\$host = 'localhost';
+\$user = '$username';
+\$pass = '$password';
+\$dbname = 'test_db';
+
+// Create connection
+\$conn = new mysqli(\$host, \$user, \$pass,\$dbname);
+
+// Check connection
+if (\$conn->connect_error) {
+    die("Connection failed: " . \$conn->connect_error);
+}
+?>
+EOF
+echo "file added sucessfully";
+sleep 2
+clear
+echo "LAMP stack and phpMyAdmin installation complete! Enjoy your setup!"
